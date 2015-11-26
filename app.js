8efd1e25ab792d81f0f7bf2ec91a66865c5213a4
FSSY <36192144@qq.com>
2015-11-26 22:44:06 +0800

    first commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 var config = require('./config');
 
 if (!config.debug && config.oneapm_key) {
-  require('oneapm');
+    require('oneapm');
 }
 
 require('colors');
@@ -47,12 +47,12 @@ var staticDir = path.join(__dirname, 'public');
 var assets = {};
 
 if (config.mini_assets) {
-  try {
-    assets = require('./assets.json');
-  } catch (e) {
-    console.log('You must execute `make build` before start app when mini_assets is true.');
-    throw e;
-  }
+    try {
+        assets = require('./assets.json');
+    } catch (e) {
+        console.log('You must execute `make build` before start app when mini_assets is true.');
+        throw e;
+    }
 }
 
 var urlinfo = require('url').parse(config.host);
@@ -71,8 +71,8 @@ app.enable('trust proxy');
 app.use(requestLog);
 
 if (config.debug) {
-  // 渲染时间
-  app.use(renderMiddleware.render);
+    // 渲染时间
+    app.use(renderMiddleware.render);
 }
 
 // 静态资源
@@ -84,18 +84,18 @@ app.use('/agent', proxyMiddleware.proxy);
 app.use(require('response-time')());
 app.use(helmet.frameguard('sameorigin'));
 app.use(bodyParser.json({limit: '1mb'}));
-app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
+app.use(bodyParser.urlencoded({extended: true, limit: '1mb'}));
 app.use(require('method-override')());
 app.use(require('cookie-parser')(config.session_secret));
 app.use(compress());
 app.use(session({
-  secret: config.session_secret,
-  store: new RedisStore({
-    port: config.redis_port,
-    host: config.redis_host,
-  }),
-  resave: true,
-  saveUninitialized: true,
+    secret: config.session_secret,
+    store: new RedisStore({
+        port: config.redis_port,
+        host: config.redis_host,
+    }),
+    resave: true,
+    saveUninitialized: true,
 }));
 
 // oauth 中间件
@@ -103,10 +103,10 @@ app.use(passport.initialize());
 
 // github oauth
 passport.serializeUser(function (user, done) {
-  done(null, user);
+    done(null, user);
 });
 passport.deserializeUser(function (user, done) {
-  done(null, user);
+    done(null, user);
 });
 passport.use(new GitHubStrategy(config.GITHUB_OAUTH, githubStrategyMiddleware));
 
@@ -115,14 +115,14 @@ app.use(auth.authUser);
 app.use(auth.blockUser());
 
 if (!config.debug) {
-  app.use(function (req, res, next) {
-    if (req.path === '/api' || req.path.indexOf('/api') === -1) {
-      csurf()(req, res, next);
-      return;
-    }
-    next();
-  });
-  app.set('view cache', true);
+    app.use(function (req, res, next) {
+        if (req.path === '/api' || req.path.indexOf('/api') === -1) {
+            csurf()(req, res, next);
+            return;
+        }
+        next();
+    });
+    app.set('view cache', true);
 }
 
 // for debug
@@ -132,22 +132,22 @@ if (!config.debug) {
 
 // set static, dynamic helpers
 _.extend(app.locals, {
-  config: config,
-  Loader: Loader,
-  assets: assets
+    config: config,
+    Loader: Loader,
+    assets: assets
 });
 
 app.use(errorPageMiddleware.errorPage);
 _.extend(app.locals, require('./common/render_helper'));
 app.use(function (req, res, next) {
-  res.locals.csrf = req.csrfToken ? req.csrfToken() : '';
-  next();
+    res.locals.csrf = req.csrfToken ? req.csrfToken() : '';
+    next();
 });
 
 app.use(busboy({
-  limits: {
-    fileSize: 10 * 1024 * 1024 // 10MB
-  }
+    limits: {
+        fileSize: 10 * 1024 * 1024 // 10MB
+    }
 }));
 
 // routes
@@ -156,21 +156,21 @@ app.use('/', webRouter);
 
 // error handler
 if (config.debug) {
-  app.use(errorhandler());
+    app.use(errorhandler());
 } else {
-  app.use(function (err, req, res, next) {
-    console.error('server 500 error:', err);
-    return res.status(500).send('500 status');
-  });
+    app.use(function (err, req, res, next) {
+        console.error('server 500 error:', err);
+        return res.status(500).send('500 status');
+    });
 }
 
 if (!module.parent) {
-  app.listen(config.port, function () {
-    logger.log('NodeClub listening on port', config.port);
-    logger.log('God bless love....');
-    logger.log('You can debug your app with http://' + config.hostname + ':' + config.port);
-    logger.log('');
-  });
+    app.listen(config.port, function () {
+        logger.log('NodeClub listening on port', config.port);
+        logger.log('God bless love....');
+        logger.log('You can debug your app with http://' + config.hostname + ':' + config.port);
+        logger.log('');
+    });
 }
 
 module.exports = app;
